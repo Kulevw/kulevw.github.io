debfc817453b969745297abd2e905a7251465c6b
Kulev Anatoly <kulevw@gmail.com>
2026-06-20 00:50:06 +0500

    add prerender routes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { ViteSSG } from 'vite-ssg'
 
+import { routes } from '@/router';
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+export const createApp = ViteSSG(App, { routes }, ({ app }) => {
+  app.use(createPinia());
+})
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,2 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from './routes'
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-})
-
-export default router
+export { routes } from './routes'
+export { RoutesNames } from './contants'
